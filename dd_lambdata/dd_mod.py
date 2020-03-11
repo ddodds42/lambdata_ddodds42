cebde622d5be774018d84a48e281b0d12d77cae1
ddodds42 <57008540+ddodds42@users.noreply.github.com>
2020-03-11 17:28:45 -0400

    Created pipenv, yet to packagize and run km_to_mile function

---
+++ dd_lambdata/dd_mod.py
@@ -0,0 +1,7 @@
+'''
+Shortcut functions: work smart, not hard!
+'''
+
+def km_to_mile(km):
+    mi = km / 1.61
+    return mi
---
+++ dd_lambdata/dd_script.py
@@ -0,0 +1,9 @@
+'''
+Executes functions defined in dd_mod.py
+'''
+
+print('wow python')
+
+# from dd_lambdata.dd_mod import km_to_mile
+
+# km_to_mile(40)
