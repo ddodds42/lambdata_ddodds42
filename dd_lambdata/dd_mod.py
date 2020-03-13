5ed82459cfb05b281764b1b2dc25b1c18d62fa9a
ddodds42 <57008540+ddodds42@users.noreply.github.com>
2020-03-12 22:18:11 -0400

    Created class to convert between SI and non-SI units

--- dd_lambdata/dd_mod.py
+++ dd_lambdata/dd_mod.py
@@ -17,3 +17,50 @@ def t_val_t(df):
     explore, train = train_test_split(df)
     test, val = train_test_split(explore)
     return explore, test, val, train
+
+class MetricConvert:
+    '''
+    This converter is designed to hopefully be useful for
+    reproducing subclasses that are useful for a wide array of
+    metric units. Temperature is the white elephant that most
+    people are familiar with, celsius and fahrenheit do not share
+    the same 0. They scale evenly, but have an awkward offset of
+    32 degrees that should be addressable with the 'int' variable (short for 'intercept'). Any other unit
+    conversion with an aligned 0 point, such as length or weight,
+    should set 'int' equal to 0.
+    '''
+    def __init__(
+                self, val_to_conv=None, int=-32,
+                old_unit="fahrenheit", coeff=(5/9),
+                si_unit="celsius"
+                ):
+        self.val_to_conv = val_to_conv
+        self.int = int
+        self.old_unit = old_unit
+        self.coeff = coeff
+        self.si_unit = si_unit
+    
+    def old_to_si(self, val_to_conv):
+        '''
+        convert the old clunky unit to S.I. (standard
+        international, or metric)
+        '''
+        c = (self.int + val_to_conv) * self.coeff
+        return print(
+                    '{} {} equals {} {}'.format(
+                        c, self.si_unit,
+                        val_to_conv, self.old_unit
+                        )
+                    )
+
+    def si_to_old(self, val_to_conv):
+        '''
+        convert the S.I. unit to the old clunky unit
+        '''
+        f = (val_to_conv / self.coeff) - self.int
+        return print(
+                    '{} {} equals {} {}'.format(
+                        f, self.old_unit,
+                        val_to_conv, self.si_unit
+                        )
+                    )
